1fe995760efb9dee137b13f1686e7daae0e715a2
SkittlezExZ <SkittlezExZ@gmail.com>
2020-03-09 11:44:27 -0400

    update swars

--- master/swars.m
+++ master/swars.m
@@ -1,11 +1,22 @@
 function [] = swars(player)
 
+global quitGame;
+quitGame = false;
+
 [mainAxis, ship, axisTitle] = initialize_graphics();
-mouseMoveListner(src, event);
-
-object = struct('outline',ship.outline,'patch',ship.patch,'w',ship.w,'h',ship.h);
-draw_object(mainAxis, object, get_mouse_position(mainAxis));
-
 print_title(axisTitle, 'SWARS');
 
-end
+
+while quitGame ~= true
+    pause(0.025);
+    
+    get_mouse_position(mainAxis);
+
+    object = struct('outline',ship.outline,'patch',ship.patch,'w',ship.w,'h',ship.h);   
+    draw_object(mainAxis, object, get_mouse_position(mainAxis));
+
+end % End for while loop
+print_title(axisTitle, 'GAME OVER');
+pause(1.0);
+close(fig);
+end % End for swars function
