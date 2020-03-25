79a98d878e1c426caded08fc00fde0989c83e027
SkittlezExZ <SkittlezExZ@gmail.com>
2020-03-25 16:12:56 -0400

    Part 2

--- master/moveObject.m
+++ master/moveObject.m
@@ -1,18 +1,49 @@
-function [shipPos] = moveObject(mousePos, shipPos, mainAxis, ship)
-    global right_button;
-    time = 50;
-    xPosition = linspace(shipPos(1),mousePos(1),time);
-    yPosition = linspace(shipPos(2),mousePos(2),time);
-    j = 0;
-while (shipPos ~= mousePos) & (j == 0)
-    for k=1:time
-        draw_object(mainAxis, ship, [xPosition(k),yPosition(k)]);
-        shipPos = [xPosition(k),yPosition(k)];
-        if right_button == true
+function [shipPos] = moveObject(shipPos, mainAxis, ship)
+global right_button;
+speed = 3.5;
+j = 0;
+delta_t = 1;
+mousePos = get_mouse_position(mainAxis);
+if player == 1
+    p1 = fopen('1.txt','w');
+    p2 = fopen('2.txt','r');
+while j == 0
+    uv = (mousePos - shipPos)/norm(mousePos - shipPos);
+    velo = speed * uv;
+    shipPos = shipPos + velo * delta_t;
+    draw_object(mainAxis, ship, shipPos)
+    
+    if shipPos(1)+1.5 > mousePos(1) && shipPos(1) - 1.5 < mousePos(1) && shipPos(2) + 1.5 > mousePos(2) && shipPos(2) - 1.5 < mousePos(2)
         j = 1;
         break
-        else
-        pause(0.025);
-        end
     end
+    if right_button == true
+        j = 1;
+        break
+    else
+        pause(0.025);
+    end
+
+end
+elseif player == 2
+    p1 = fopen('1.txt','r');
+    p2 = fopen('2.txt','w');
+while j == 0
+    uv = (mousePos - shipPos)/norm(mousePos - shipPos);
+    velo = speed * uv;
+    shipPos = shipPos + velo * delta_t;
+    draw_object(mainAxis, ship, shipPos)
+    
+    if shipPos(1)+1.5 > mousePos(1) && shipPos(1) - 1.5 < mousePos(1) && shipPos(2) + 1.5 > mousePos(2) && shipPos(2) - 1.5 < mousePos(2)
+        j = 1;
+        break
+    end
+    if right_button == true
+        j = 1;
+        break
+    else
+        pause(0.025);
+    end
+
+end
 end
--- master/swars.m
+++ master/swars.m
@@ -1,25 +1,27 @@
 function [] = swars(player)
 
-clear;clc
+%     p1 = fopen('1.txt','r');
+%     p2 = fopen('2.txt','w');
 
+    
 global quitGame;
-quitGame = false;
 global right_button;
+quitGame = false;
 
 [mainAxis, ship, axisTitle] = initialize_graphics();
 print_title(axisTitle, 'SWARS');
-shipPos = [100 50];
-draw_object(mainAxis, ship, shipPos);
+
 while ~quitGame    
-pause(0.025);
-    if right_button == true
-        right_button = false;
-        mousePos = get_mouse_position(mainAxis);
-        [shipPos] = moveObject(mousePos,shipPos, mainAxis, ship);
+    if player == 1
+        pause(0.025);
+        
+        if right_button == true
+            right_button = false;
+            [shipPos] = moveObject(shipPos, mainAxis, ship);
+        end
     end
-
 end % End while loop
-
 print_title(axisTitle, 'GAME OVER');
 
 end % End for swars function
+
