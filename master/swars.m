c4ec71417c905c51730a4a225861d48a060361d3
dherrerac <61883285+dherrerac@users.noreply.github.com>
2020-04-08 12:21:43 -0400

    Update swars.m

--- master/swars.m
+++ master/swars.m
@@ -10,6 +10,20 @@ quitGame = false;
 [mainAxis, ship, axisTitle] = initialize_graphics();
 print_title(axisTitle, 'SWARS');
 
+TORP_START_Y = 20; %torp start position
+torp.w = 5; %torp width
+torp.h = 10; %torp height
+TORP_SHAPE = [1 0 1 2 3 4 3 1; ... %x values
+  0 1 2 4 2 1 0 0];    %y values
+xScale = torp.w / max(TORP_SHAPE(1,:));
+yScale = torp.h / max(TORP_SHAPE(2,:));
+
+%coordinats for drawing torp at 0,0.
+%scale torp so that he's TORP_W wide and TORP_H tall
+torp.outline = [TORP_SHAPE(1,:) .* xScale; TORP_SHAPE(2,:) .* yScale];
+
+torpPos = [100 150];
+
 while ~quitGame
     
     pause(0.025);
