7543f8f93a0388438ebfc0233041dc731391928b
SkittlezExZ <SkittlezExZ@gmail.com>
2020-04-24 10:58:05 -0400

    Cleanup

--- master/isCollidingCheck.m
+++
@@ -1,5 +0,0 @@
-function isCollidingCheck ()
-if ship1Pos(1)+2 > mousePos(1) && ship1Pos(1) - 2 < mousePos(1) && ship1Pos(2) + 2 > mousePos(2) && ship1Pos(2) - 2 < mousePos(2)
-                j = 1;
-                break
-            end
--- master/test.m
+++
@@ -1,7 +0,0 @@
-x = [1 2 3 4; 6 7 8 9; 10 11 12 13; 0 0 0 0];
-writematrix(x,'test.txt','Delimiter','space');
-
-fid = fopen('test.txt', 'r');
-torpArr = fscanf(fid, '%f', [4 inf])';
-fclose(fid);
-disp(torpArr);
